8f93961ffbf148eb9dd022869b4892b732481dd0
r0_nook <wodrom@gmail.com>
2021-05-21 15:54:07 +0300

    tdm

--- r0_js/mate.js
+++ r0_js/mate.js
@@ -1,28 +1,12 @@
 'use strict';
+const users = {
+    fullName = undifine,
+    firstName = "R0",
+    lastName = "R1"
+}
+function restoreNames(users) {
 
-/**
- * @param {string} guestsInput
- *
- * @returns {number|string}
- */
-
-function calculateGuests(guestsInput) {
-  const r;
-
-  for (let i = 0; i < guestsInput.lenght; i++){
-    function isPrime(element, index, array) {
-    var start = 2;
-    while (start <= Math.sqrt(element)) {
-    if (element % start++ < 1) {
-    return false;
-    }
-    }
-    return element > 1;
-    }
-    
-    r = guestsInput.split();
-    return r.find(isPrime)
-    
-    // return 
-
-  }
+  if (users.firstName === undefined) {
+    users.firstName = "1";
+  };
+}
