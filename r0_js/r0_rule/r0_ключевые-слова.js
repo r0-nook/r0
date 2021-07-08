563f64ec19dc3e2fb4bd2d22cce80321f3cf99ae
R0 <wodrom@gmail.com>
2021-07-08 15:11:35 +0300

    moldcalm

---
+++ r0_js/r0_rule/r0_ключевые-слова.js
@@ -0,0 +1,12 @@
+Ключевые слова JavaScript :
+
+break, delete, function, return, typeof, case, do, if, switch, var, catch, else, in, this, void, continue, false, instanceof, throw, while, debugger, finally, new, true, with, default, for, null, try
+
+Также в JavaScript есть зарезервированные слова, не являющиеся частью языка, но могущие войти в него в будущем (мы рассматриваем стандарт ECMA-262):
+
+class, const, enum, export, extends, import, super
+
+Также не рекомендуется, а в некоторых случаях и не разрешается использовать в качестве идентификаторов следующие слова:
+
+implements, let, private, public, yield, interface, package, protected, static
+
--- r0_js/r0_test/script.js
+++ r0_js/r0_test/script.js
@@ -1,16 +1,6 @@
 'use strict';
 debugger;
-
-	let numbers = [1, 3, 1, 5, 2, 5, 2, 6, 3];
-
-	const rMax = [];
-	let r5 = 5, rNumbers = [...numbers];
-
-	while (r5) {
-		const r0 = rNumbers.reduce((a,b)=>a>b?a:b);
-
-		r5--; rMax.push(r0);
-		rNumbers.splice(rNumbers.indexOf(r0), 1);
-	}
-
-	const r1 = rMax;
+const numbers = [1, 2, 3, 4, 5];
+const [y] = numbers;
+console.log(y);
+ВЫФСЫ
